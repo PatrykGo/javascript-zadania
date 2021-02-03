d722381b521df1d7231a63240b07cc2d48236eb6
Patryk Górski <72337717+PatrykGo@users.noreply.github.com>
2021-02-03 16:57:39 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//zad1
+console.log("Patryk");
+
+//zad2
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+//zad3
+console.log("Hello World"); 
+//console.log(Hello World); //brak ""
+//console.log("Hello World); //brak " na końcu tekstu
