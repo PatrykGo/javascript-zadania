068f8bf9292ea311609592c917a55a8cf08d5cd5
Patryk Górski <72337717+PatrykGo@users.noreply.github.com>
2021-02-03 17:00:45 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,34 @@
+//zad1
+var a = true, b = false;
+console.log(a === b);
+
+//zad2
+var a = 36, b = 12, moduloResult = 0;
+moduloResult = a / b;
+console.log(moduloResult);
+
+//zad3
+var a = "Piotr", b = "Kowalski", stringResult = "";
+stringResult = a + " " + b;
+console.log(stringResult);
+
+//zad4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString); // true
+console.log(someString === someNumber); // false
+
+// operator '==' porównuje czy zmienne są równe, nie zwracając uwagi na typ
+// operator '===' porównuje tez typ zmiennych 
+
+//zad5
+var counter = 30;
+console.log(counter);
+console.log(++counter);
+console.log(--counter);
+
+//zad6
+var a = 12, b = 34, result = null;
+result = a > b;
+console.log(result);
