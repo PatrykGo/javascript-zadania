ed2d5d7a943adba9358dca939a25a7c6e159dcd0
Patryk Górski <72337717+PatrykGo@users.noreply.github.com>
2021-02-03 16:58:31 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,21 @@
+//zad1
+var num = 6;
+var string = "string";
+var num_string = 8 + "string again";
+var logic = true;
+var special = {test: "yes"};
+
+console.log(num);
+console.log(string);
+console.log(num_string);
+console.log(logic);
+console.log(special);
+
+//zad2
+var a = 34, b = 61, sum = 0;
+console.log(sum = a + b);
+
+//zad3
+var nothing;
+console.log(nothing);
+// undefined, zmienna nie została zdefiniowana
